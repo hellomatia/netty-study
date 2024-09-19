a815d3974f51e1af389a1c24c3569e243c282f7d
hellomatia <shuaigejp80@gmail.com>
2024-09-19 16:46:16 +0900

    [spring-reactive] DeferredResult를 활용한 비동기 응답 기능 추가

--- spring-reactive-programing/src/main/java/com/study/reactive/ReactiveStudyApplication.java
+++ spring-reactive-programing/src/main/java/com/study/reactive/ReactiveStudyApplication.java
@@ -9,12 +9,14 @@ import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
+import java.util.Queue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 @SpringBootApplication
 @Slf4j
@@ -23,6 +25,30 @@ public class ReactiveStudyApplication {
 
     @RestController
     public static class MyController {
+        Queue<DeferredResult<String>> results = new ConcurrentLinkedQueue<>();
+
+        @GetMapping("/dr")
+        public DeferredResult<String> dr() throws InterruptedException {
+            log.info("deferred result");
+            DeferredResult<String> dr = new DeferredResult<>();
+            results.add(dr);
+            return dr;
+        }
+
+        @GetMapping("/dr/count")
+        public String drCount() {
+            return String.valueOf(results.size());
+        }
+
+        @GetMapping("/dr/event")
+        public String drEvent(String msg) {
+            for (DeferredResult<String> dr : results) {
+                dr.setResult("Hello " + msg);
+                results.remove(dr);
+            }
+            return "OK";
+        }
+
         @GetMapping("/callable")
         public Callable<String> callable() throws InterruptedException {
             log.info("callable");
