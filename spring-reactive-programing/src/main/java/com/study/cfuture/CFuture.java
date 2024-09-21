c55b7b975f5474ce66beb475a58fba5d2d2309a5
hellomatia <shuaigejp80@gmail.com>
2024-09-21 23:44:24 +0900

    [spring-reactive] CompletableFuture Async 예제 추가

--- spring-reactive-programing/src/main/java/com/study/cfuture/CFuture.java
+++ spring-reactive-programing/src/main/java/com/study/cfuture/CFuture.java
@@ -2,33 +2,33 @@ package com.study.cfuture;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 @Slf4j
 public class CFuture {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
         CompletableFuture
                 .supplyAsync(() -> {
                     log.info("supplyAsync");
-                    if (1 == 1) throw new RuntimeException();
                     return 1;
                 })
                 .thenCompose(s -> {
                     log.info("thenCompose {}", s);
                     return CompletableFuture.completedFuture(s + 1);
                 })
-                .thenApply(s2 -> {
-                    log.info("thenApply {}", s2);
+                .thenApplyAsync(s2 -> {
+                    log.info("thenApplyAsync {}", s2);
                     return s2 + 1;
-                })
+                }, executorService)
                 .exceptionally(e -> -10)
-                .thenAccept(s3 -> log.info("thenAccept {}", s3));
+                .thenAcceptAsync(s3 -> log.info("thenAccept {}", s3), executorService);
         log.info("Exit");
 
         ForkJoinPool.commonPool().shutdown();
         ForkJoinPool.commonPool().awaitTermination(10, TimeUnit.SECONDS);
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.SECONDS);
     }
 }
