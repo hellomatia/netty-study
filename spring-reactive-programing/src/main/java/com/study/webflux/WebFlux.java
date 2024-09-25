55cef469ab5b377684f0c643653b2528070ae112
hellomatia <shuaigejp80@gmail.com>
2024-09-25 23:22:24 +0900

    [spring-reactive-programing] Mono를 반환하여 비동기 작업을 처리하는 Controller 추가

---
+++ spring-reactive-programing/src/main/java/com/study/webflux/WebFlux.java
@@ -0,0 +1,40 @@
+package com.study.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.stream.Stream;
+
+@EnableWebFlux
+@SpringBootApplication
+public class WebFlux {
+
+    public static void main(String[] args) {
+        System.setProperty("spring.main.web-application-type", "reactive");
+        SpringApplication.run(WebFlux.class, args);
+    }
+
+    @RestController
+    static class MyController {
+        WebClient client = WebClient.create();
+
+        @GetMapping("/webflux")
+        public Mono<String> webflux(int idx) {
+            // Mono는 Publisher이다.
+            // 비동기 작업을 수행하고 Publisher하는 역할을 하는거다.
+            // 하지만 누가 Subscribe를 하지 않으면 Publisher하지 않는다.
+            Mono<ClientResponse> responseMono = client.get()
+                    .uri("http://localhost:8081/service1?req={req}", idx)
+                    .exchange();
+            return responseMono
+                    .flatMap(clientResponse -> clientResponse.bodyToMono(String.class));
+        }
+    }
+}
+
